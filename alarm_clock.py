0d6028c640c0c87b7b36bb6fa055a6656c96ca73
Parq254 <juniorchris761@gmail.com>
2024-06-15 07:06:42 +0300

    Eleventh project

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,10 +1,8 @@
 from playsound import playsound
 import time
 
-
-
-CLEAR = '/033[2J'
-CLEAR_AND_RETURN = '\033[H'
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
 
 
 def alarm(seconds):
@@ -19,11 +17,10 @@ def alarm(seconds):
         minutes_left = time_left // 60
         seconds_left = time_left % 60
 
-        print(f'{CLEAR_AND_RETURN}{minutes_left:02d}:{seconds}')
-    playsound('alarm.mp3')
+        print(f"{CLEAR_AND_RETURN}Alarm will sound in :{minutes_left:02d}:{seconds_left:02d}")
 
 
 minutes = int(input('How many minutes to wait: '))
 seconds = int(input('How many seconds to wait: '))
 total_seconds = minutes * 60 + seconds
-alarm(10)
+alarm(total_seconds)
