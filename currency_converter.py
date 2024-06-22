85240abe3a860380274c60be21eba25a005a0c91
Parq254 <juniorchris761@gmail.com>
2024-06-22 09:19:05 +0300

    seventeenth project

--- currency_converter.py
+++ currency_converter.py
@@ -1,40 +1,88 @@
 from requests import get
 from pprint import PrettyPrinter
 
-BASE_URL = "https://api.currencyapi.com"
-API_KEY = "cur_live_vAsg8euOYay1p0iMST2aE9mv1WtQNFDaLLpH3nYv"
+BASE_URL = "https://free.currconv.com/"
+API_KEY = "29cc41bbdc271ff812d"
 
 printer = PrettyPrinter()
 
 
 def get_currencies():
-    endpoint = f'/v3/currencies?apikey={API_KEY}'
+    endpoint = f"api/v7/currencies?apiKey={API_KEY}"
     url = BASE_URL + endpoint
-    _data = get(url).json()['data']
+    data = get(url).json()['results']
 
-    _data = list(_data.items())
-    _data.sort()
+    data = list(data.items())
+    data.sort()
 
-    return _data
+    return data
 
 
 def print_currencies(currencies):
     for name, currency in currencies:
-        name = currency['name']
-        symbol = currency['symbol']
-        print(f'{name} - {symbol}')
+        name = currency['currencyName']
+        _id = currency['id']
+        symbol = currency.get("currencySymbol", "")
+        print(f"{_id} - {name} - {symbol}")
 
 
-def exchange_rate(code1, code2):
-    endpoint = f"https://api.currencyapi.com/v3/convert?value=12 -H apikey:cur_live_vAsg8euOYay1p0iMST2aE9mv1WtQNFDaLLpH3nYv "
-
+def exchange_rate(currency1, currency2):
+    endpoint = f"api/v7/convert?q={currency1}_{currency2}&compact=ultra&apiKey={API_KEY}"
     url = BASE_URL + endpoint
-    response = get(url)
+    data = get(url).json()
 
-    _data = response.json()
-    printer.pprint(_data)
+    if len(data) == 0:
+        print('Invalid currencies.')
+        return
+
+    rate = list(data.values())[0]
+    print(f"{currency1} -> {currency2} = {rate}")
+
+    return rate
 
 
-# _data = get_currencies()
-# print_currencies(_data)
-exchange_rate('XRP', 'ADA')
+def convert(currency1, currency2, amount):
+    rate = exchange_rate(currency1, currency2)
+    if rate is None:
+        return
+
+    try:
+        amount = float(amount)
+    except:
+        print("Invalid amount.")
+        return
+
+    converted_amount = rate * amount
+    print(f"{amount} {currency1} is equal to {converted_amount} {currency2}")
+    return converted_amount
+
+
+def main():
+    currencies = get_currencies()
+
+    print("Welcome to the currency converter!")
+    print("List - lists the different currencies")
+    print("Convert - convert from one currency to another")
+    print("Rate - get the exchange rate of two currencies")
+    print()
+
+    while True:
+        command = input("Enter a command (q to quit): ").lower()
+
+        if command == "q":
+            break
+        elif command == "list":
+            print_currencies(currencies)
+        elif command == "convert":
+            currency1 = input("Enter a base currency: ").upper()
+            amount = input(f"Enter an amount in {currency1}: ")
+            currency2 = input("Enter a currency to convert to: ").upper()
+            convert(currency1, currency2, amount)
+        elif command == "rate":
+            currency1 = input("Enter a base currency: ").upper()
+            currency2 = input("Enter a currency to convert to: ").upper()
+            exchange_rate(currency1, currency2)
+        else:
+            print("Unrecognized command!")
+
+main()
